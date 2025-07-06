1195e332c57247d07541201e0081e1b7ffaafb01
Hamza84-prog <hamzajarrar84@gmail.com>
2025-07-06 23:15:17 +0300

    تعديل على السيدبار

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -70,18 +70,39 @@
 					</li>
 					
 					<li class="side-item side-item-category">الفواتير</li>
+
+					<!-- فواتير المبيعات -->
 					<li class="slide">
-						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 11V4H4v8.17l.59-.58.58-.59H6z" opacity=".3"/><path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-5 7c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10zM4.59 11.59l-.59.58V4h11v7H5.17l-.58.59z"/></svg><span class="side-menu__label">فواتير المبيعات</span><i class="angle fe fe-chevron-down"></i></a>
-						<ul class="slide-menu">
-							<li><a class="slide-item" href="{{ route('invoices.index') }}">قائمة فواتير المبيعات</a></li>
-							<li><a class="slide-item" href="{{ route('invoices.create') }}">إضافة فاتورة مبيعات جديدة</a></li>
-						</ul>
-						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 11V4H4v8.17l.59-.58.58-.59H6z" opacity=".3"/><path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-5 7c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10zM4.59 11.59l-.59.58V4h11v7H5.17l-.58.59z"/></svg><span class="side-menu__label">فواتير المشتريات</span><i class="angle fe fe-chevron-down"></i></a>
-						<ul class="slide-menu">
-							<li><a class="slide-item" href="{{ route('purchase_invoices.index') }}">قائمة فواتير المشتريات</a></li>
-							<li><a class="slide-item" href="{{ route('purchase_invoices.create') }}">إضافة فاتورة مشتريات جديدة</a></li>
-						</ul>
+					<a class="side-menu__item" data-toggle="slide" href="#">
+						<!-- أيقونة مبيعات (فاتورة/تقارير) -->
+						<svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-6h6v6m2 4H7a2 2 0 01-2-2V5a2 2 0 012-2h5l2 2h5a2 2 0 012 2v12a2 2 0 01-2 2z" />
+						</svg>
+						<span class="side-menu__label">فواتير المبيعات</span>
+						<i class="angle fe fe-chevron-down"></i>
+					</a>
+					<ul class="slide-menu">
+						<li><a class="slide-item" href="{{ route('invoices.index') }}">قائمة فواتير المبيعات</a></li>
+						<li><a class="slide-item" href="{{ route('invoices.create') }}">إضافة فاتورة مبيعات جديدة</a></li>
+					</ul>
 					</li>
+
+					<!-- فواتير المشتريات -->
+					<li class="slide">
+					<a class="side-menu__item" data-toggle="slide" href="#">
+						<!-- أيقونة مشتريات (عربة شراء) -->
+						<svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.2 6h12.4M7 13H3M16 16a2 2 0 100 4 2 2 0 000-4zm-8 0a2 2 0 100 4 2 2 0 000-4z" />
+						</svg>
+						<span class="side-menu__label">فواتير المشتريات</span>
+						<i class="angle fe fe-chevron-down"></i>
+					</a>
+					<ul class="slide-menu">
+						<li><a class="slide-item" href="{{ route('purchase_invoices.index') }}">قائمة فواتير المشتريات</a></li>
+						<li><a class="slide-item" href="{{ route('purchase_invoices.create') }}">إضافة فاتورة مشتريات جديدة</a></li>
+					</ul>
+					</li>
+
 					<li class="side-item side-item-category">المستخدمين</li>
 					<li class="slide">
 						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 11V4H4v8.17l.59-.58.58-.59H6z" opacity=".3"/><path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-5 7c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10zM4.59 11.59l-.59.58V4h11v7H5.17l-.58.59z"/></svg><span class="side-menu__label">إدارة سجلات الانشطة</span><i class="angle fe fe-chevron-down"></i></a>
@@ -130,46 +151,7 @@
 
 						</ul>
 					</li>
-					<li class="slide">
-						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10.9 19.91c.36.05.72.09 1.1.09 2.18 0 4.16-.88 5.61-2.3L14.89 13l-3.99 6.91zm-1.04-.21l2.71-4.7H4.59c.93 2.28 2.87 4.03 5.27 4.7zM8.54 12L5.7 7.09C4.64 8.45 4 10.15 4 12c0 .69.1 1.36.26 2h5.43l-1.15-2zm9.76 4.91C19.36 15.55 20 13.85 20 12c0-.69-.1-1.36-.26-2h-5.43l3.99 6.91zM13.73 9h5.68c-.93-2.28-2.88-4.04-5.28-4.7L11.42 9h2.31zm-3.46 0l2.83-4.92C12.74 4.03 12.37 4 12 4c-2.18 0-4.16.88-5.6 2.3L9.12 11l1.15-2z" opacity=".3"/><path d="M12 22c5.52 0 10-4.48 10-10 0-4.75-3.31-8.72-7.75-9.74l-.08-.04-.01.02C13.46 2.09 12.74 2 12 2 6.48 2 2 6.48 2 12s4.48 10 10 10zm0-2c-.38 0-.74-.04-1.1-.09L14.89 13l2.72 4.7C16.16 19.12 14.18 20 12 20zm8-8c0 1.85-.64 3.55-1.7 4.91l-4-6.91h5.43c.17.64.27 1.31.27 2zm-.59-3h-7.99l2.71-4.7c2.4.66 4.35 2.42 5.28 4.7zM12 4c.37 0 .74.03 1.1.08L10.27 9l-1.15 2L6.4 6.3C7.84 4.88 9.82 4 12 4zm-8 8c0-1.85.64-3.55 1.7-4.91L8.54 12l1.15 2H4.26C4.1 13.36 4 12.69 4 12zm6.27 3h2.3l-2.71 4.7c-2.4-.67-4.35-2.42-5.28-4.7h5.69z"/></svg><span class="side-menu__label">Utilities</span><i class="angle fe fe-chevron-down"></i></a>
-						<ul class="slide-menu">
-							<li><a class="slide-item" href="{{ url('/' . $page='background') }}">Background</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='border') }}">Border</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='display') }}">Display</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='flex') }}">Flex</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='height') }}">Height</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='margin') }}">Margin</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='padding') }}">Padding</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='position') }}">Position</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='width') }}">Width</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='extras') }}">Extras</a></li>
-						</ul>
-					</li>
-					<li class="slide">
-						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 20h12V10H6v10zm6-7c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z" opacity=".3"/><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/></svg><span class="side-menu__label">Custom Pages</span><i class="angle fe fe-chevron-down"></i></a>
-						<ul class="slide-menu">
-							<li><a class="slide-item" href="{{ url('/' . $page='signin') }}">Sign In</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='signup') }}">Sign Up</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='forgot') }}">Forgot Password</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='reset') }}">Reset Password</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='lockscreen') }}">Lockscreen</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='underconstruction') }}">UnderConstruction</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='404') }}">404 Error</a></li>
-							<li><a class="slide-item" href="{{ url('/' . $page='500') }}">500 Error</a></li>
-						</ul>
-					</li>
-					<li class="slide ">
-						<a class="side-menu__item" data-toggle="slide" href="{{ url('/' . $page='#') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 9h14V5H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5S7.83 8.5 7 8.5 5.5 7.83 5.5 7 6.17 5.5 7 5.5zM5 19h14v-4H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5z" opacity=".3"/><path d="M20 13H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zm-1 6H5v-4h14v4zm-12-.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zM20 3H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zm-1 6H5V5h14v4zM7 8.5c.83 0 1.5-.67 1.5-1.5S7.83 5.5 7 5.5 5.5 6.17 5.5 7 6.17 8.5 7 8.5z"/></svg><span class="side-menu__label">Submenus</span><i class="angle fe fe-chevron-down"></i></a>
-						<ul class="slide-menu">
-							<li class="sub-slide">
-								<a class="sub-side-menu__item" data-toggle="sub-slide" href="{{ url('/' . $page='#') }}"><span class="sub-side-menu__label">Level1</span><i class="sub-angle fe fe-chevron-down"></i></a>
-								<ul class="sub-slide-menu">
-									<li><a class="sub-slide-item" href="{{ url('/' . $page='#') }}">Level01</a></li>
-									<li><a class="sub-slide-item" href="{{ url('/' . $page='#') }}">Level02</a></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
+					
 				</ul>
 			</div>
 		</aside>
